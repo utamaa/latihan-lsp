14c1fca99140813486d8faca07a72708992aae30
Utama <99374352+utamaa@users.noreply.github.com>
2022-09-25 14:04:52 +0700

    Update dbconnect.php

--- crud-practice/dbconnect.php
+++ crud-practice/dbconnect.php
@@ -2,9 +2,11 @@
     $username = 'root';
     $password = '';
     $servername = 'localhost';
+    $bdName = "latihan_users";
 
-    $conn = mysqli_connect($servername,$username,$password);
+    $conn = mysqli_connect($servername,$username,$password,$bdName);
     if(!$conn) {
         die("unable to connect" . mysqli_connect_error());
     }
 ?>
+Footer
