c89daca27f8a1b7b5ecad824906387c244678a0d
Utama <99374352+utamaa@users.noreply.github.com>
2022-09-25 14:06:16 +0700

    Update sign-up.php

--- crud-practice/sign-up.php
+++ crud-practice/sign-up.php
@@ -3,22 +3,22 @@
     session_start();
     require "dbconnect.php";
 
-    if(isset($_POST['sign up'])) {
-        $name = mysqli_real_escape_string($conn, $_POST['UserName']);
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $pass = mysqli_real_escape_string($conn, $_POST['password']);
+    if(isset($_POST["sign-up"])) {
+        $name = mysqli_real_escape_string($conn, $_POST["UserName"]);
+        $email = mysqli_real_escape_string($conn, $_POST["email"]);
+        $pass = mysqli_real_escape_string($conn, $_POST["password"]);
     
-        $query = "INSERT INTO tabel (name,email,password) VALUES('$name','$email','$pass')";
+        $query = "INSERT INTO users (name,email,password) VALUES('$name','$email','$pass')";
         $query_run = mysqli_query($conn,$query);
     
         if($query_run){
             $_SESSION['message'] = 'Sign Up successfull'; 
             header("Location: form.php");
-            exit(0);
+            exit();
         } else {
-            $_SESSION['message'] = 'Sign Up successfull'; 
+            $_SESSION['message'] = 'Sign Up failed'; 
             header("Location: form.php");
-            exit(0);
+            exit();
         }
     }
 
